aefadac985939f51e7260d6a67404834c741a949
S.KAMOUN <said.kamoun@gmail.com>
2023-01-18 15:59:50 +0100

    create  secret key

--- src/main/java/com/kamoun/security/config/JwtService.java
+++ src/main/java/com/kamoun/security/config/JwtService.java
@@ -2,10 +2,15 @@ package com.kamoun.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static  final  String SECRET_KEY="DWEXGZH2J4M5N6P8R9SAUCVDWFYGZH3K4M5P7Q8RATBUCWEXFYH2J3K4N6";
     public String extractUsername(String jwt){
         return  null;
     }
@@ -17,7 +22,8 @@ public class JwtService {
                 .getBody();
     }
 
-    private byte[] getSigningkey() {
-        return  null;
+    private Key getSigningkey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
